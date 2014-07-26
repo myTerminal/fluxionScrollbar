105f04cd85d9d05ca82e9829ed8672dd358515df
myTerminal <team.terminal@gmail.com>
2014-07-26 16:22:05 +0530

    Modified installer.js

--- installer.js
+++ installer.js
@@ -1,15 +1,15 @@
 require('package-script').spawn([
     {
         command: "npm",
-        args: ["install", "-g", "grunt-cli"]
-    },
-    {
-        command: "npm",
         args: ["install", "-g", "npm-check-updates"]
     },
     {
         command: "npm",
         args: ["install", "-g", "istanbul"]
+    },
+    {
+        command: "npm",
+        args: ["install", "-g", "grunt-cli"]
     },
     {
         command: "npm",
